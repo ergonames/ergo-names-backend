59d98814fe1de89e9e4cb9507d530f780e463a2e
JC Gonzalez <programmer1718@gmail.com>
2022-02-21 01:48:16 -0500

    implement SubmitMintingRequest scenario

---
+++ src/main/scala/scenarios/SubmitMintingRequest.scala
@@ -0,0 +1,52 @@
+package scenarios
+
+import org.ergoplatform.appkit._
+import org.ergoplatform.appkit.config.ErgoToolConfig
+import utils.ErgoNamesUtils
+
+object SubmitMintingRequest {
+  def submitMintingRequest(conf: ErgoToolConfig, networkType: NetworkType): String = {
+    val ergoClient = ErgoNamesUtils.buildErgoClient(conf.getNode, networkType)
+
+    val mintingContractAddress = Address.create(conf.getParameters.get("mintingContractAddress"))
+    val royaltyPercentage = conf.getParameters.get("royaltyPercentage").toInt
+    val tokenName = conf.getParameters.get("tokenName")
+    val paymentAmount = conf.getParameters.get("paymentAmount").toLong
+    val nftReceiverAddress = Address.create(conf.getParameters.get("nftReceiverAddress"))
+
+    val txJson: String = ergoClient.execute((ctx: BlockchainContext) => {
+        val senderProver = ErgoNamesUtils.buildProver(ctx, conf.getNode)
+
+        val totalToSpend = paymentAmount + Parameters.MinFee
+        val boxesToSpend = ErgoNamesUtils.getBoxesToSpendFromWallet(ctx, totalToSpend)
+        if (!boxesToSpend.isPresent)
+          throw new ErgoClientException(s"Not enough coins in the wallet to pay $totalToSpend", null)
+
+        val mintingRequestBox = ErgoNamesUtils.buildMintingRequestBox(
+          ctx,
+          mintingContractAddress,
+          royaltyPercentage,
+          tokenName,
+          paymentAmount,
+          nftReceiverAddress)
+
+        val tx = ErgoNamesUtils.buildUnsignedTx(
+          ctx,
+          inputs = boxesToSpend.get,
+          outputs = mintingRequestBox,
+          fee = Parameters.MinFee,
+          changeAddress = senderProver.getP2PKAddress)
+
+        val signedTx = senderProver.sign(tx)
+        val txId = ctx.sendTransaction(signedTx)
+        signedTx.toJson(true)
+    })
+    txJson
+  }
+
+  def main(args: Array[String]) : Unit = {
+    val conf: ErgoToolConfig = ErgoToolConfig.load("config.json")
+    val txJson = submitMintingRequest(conf, NetworkType.TESTNET)
+    print(txJson)
+  }
+}
