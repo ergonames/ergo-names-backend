9b341c30e96bf3d76ec5a0698271a4b7e094c2df
JC Gonzalez <programmer1718@gmail.com>
2022-02-21 01:28:47 -0500

    implement DeployMintingContract scenario

---
+++ src/main/scala/scenarios/DeployMintingContract.scala
@@ -0,0 +1,45 @@
+package scenarios
+
+import contracts.ErgoNamesMintingContract
+import org.ergoplatform.appkit.config.ErgoToolConfig
+import org.ergoplatform.appkit._
+import utils.ErgoNamesUtils
+
+object DeployMintingContract {
+  def deployMintingContract(conf: ErgoToolConfig, networkType: NetworkType): String = {
+    val ergoClient = ErgoNamesUtils.buildErgoClient(conf.getNode, networkType)
+
+    val txJson: String = ergoClient.execute((ctx: BlockchainContext) => {
+        val senderProver = ErgoNamesUtils.buildProver(ctx, conf.getNode)
+
+        val totalToSpend = Parameters.MinChangeValue + Parameters.MinFee
+        val boxesToSpend = ErgoNamesUtils.getBoxesToSpendFromWallet(ctx, totalToSpend)
+        if (!boxesToSpend.isPresent)
+          throw new ErgoClientException(s"Not enough coins in the wallet to pay $totalToSpend", null)
+
+        val contractBox: OutBox = ErgoNamesUtils.buildContractBox(
+          ctx,
+          amountToSend = Parameters.MinChangeValue,
+          script = ErgoNamesMintingContract.getScript,
+          ergoNamesPk = senderProver.getP2PKAddress.pubkey)
+
+        val tx: UnsignedTransaction = ErgoNamesUtils.buildUnsignedTx(
+          ctx,
+          inputs = boxesToSpend.get,
+          outputs = contractBox,
+          fee = Parameters.MinFee,
+          changeAddress = senderProver.getP2PKAddress)
+
+        val signedTx = senderProver.sign(tx)
+        val txId = ctx.sendTransaction(signedTx)
+        signedTx.toJson(true)
+    })
+    txJson
+  }
+
+  def main(args: Array[String]) : Unit = {
+    val conf: ErgoToolConfig = ErgoToolConfig.load("config.json")
+    val txJson = deployMintingContract(conf, NetworkType.TESTNET)
+    print(txJson)
+  }
+}
