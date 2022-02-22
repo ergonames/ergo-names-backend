93a5e05a19544a1848ca63958da6736465457483
David Przybilla <dav.alejandro@gmail.com>
2022-02-22 11:31:34 +0900

    adding test case

--- src/test/scala/scenarios/ScenariosSpec.scala
+++ src/test/scala/scenarios/ScenariosSpec.scala
@@ -10,7 +10,9 @@ import org.ergoplatform.P2PKAddress
 import org.scalatest.{ PropSpec, Matchers }
 import org.ergoplatform.ErgoAddressEncoder
 import org.scalatest._
+import org.scalatest.Assertions._
 import org.scalatest.{ Matchers, WordSpecLike }
+import sigmastate.lang.exceptions.InterpreterException
 
 
 class WorkflowSpec extends WordSpecLike with Matchers {
@@ -24,9 +26,11 @@ class WorkflowSpec extends WordSpecLike with Matchers {
 
       val ergoNames = blockchainSim.newParty("ergoNames", ctx)
       val customer = blockchainSim.newParty("customer", ctx)
+      val other = blockchainSim.newParty("other", ctx)
       val initialAmount = 100000000000000L
       ergoNames.generateUnspentBoxes(toSpend = initialAmount)
       customer.generateUnspentBoxes(toSpend = initialAmount)
+      other.generateUnspentBoxes(toSpend = initialAmount)
 
       // creates a minting contract address on which requests can be added
       val amountToSpend = Parameters.MinChangeValue + Parameters.MinFee
@@ -55,17 +59,27 @@ class WorkflowSpec extends WordSpecLike with Matchers {
       val signedsubmitTx = customer.wallet.sign(submitTx)
       ctx.sendTransaction(signedsubmitTx)
 
-      // processing a mint request
+
+
+      // preparing data for a mint request
       val tokenDesc = "Early stage testing of token minting with contracts"
+      val contractBoxes =  ctx.getUnspentBoxesFor(mintingContractAddress, 0, Int.MaxValue)
+      val mintRequestBox = contractBoxes.get(0)
+
+      // an entity aside from ergonames should not be able to mint
+      val otherBoxes = other.wallet.getUnspentBoxes(Parameters.MinFee+ Parameters.MinChangeValue+100000).get
+      val mintTxshoudFail = ProcessMintingRequest.createTx(ctx, otherBoxes, other.wallet.getAddress, mintRequestBox, tokenDesc, NetworkType.MAINNET)    
+     assertThrows[InterpreterException]{
+        val signedmintTxshouldFail = other.wallet.sign(mintTxshoudFail)
+        ctx.sendTransaction(signedmintTxshouldFail)
+     }
+
+      // ergonames tries to mint then it should be ok
       val boxesToSpendForMint = ergoNames.wallet.getUnspentBoxes(Parameters.MinFee+ Parameters.MinChangeValue+100000).get
-      
-      val a =  ctx.getUnspentBoxesFor(mintingContractAddress, 0, Int.MaxValue)
-      val mintRequestBox = a.get(0)
-      
       val mintTx =ProcessMintingRequest.createTx(ctx, boxesToSpendForMint, ergoNames.wallet.getAddress, mintRequestBox, tokenDesc, NetworkType.MAINNET)
       val signedmintTx = ergoNames.wallet.sign(mintTx)
       ctx.sendTransaction(signedmintTx)
-
+       
 
     })
   }
