012976dee44b4e8949ff7f7f95d214adff9dd5ab
David Przybilla <dav.alejandro@gmail.com>
2022-02-27 20:36:27 +0900

    sqs lambda handler

--- src/main/scala/scenarios/ProcessMintingRequest.scala
+++ src/main/scala/scenarios/ProcessMintingRequest.scala
@@ -1,12 +1,22 @@
 package scenarios
 
+import com.amazonaws.services.lambda.runtime.Context
+import com.amazonaws.services.lambda.runtime.events.{SQSEvent}
+import play.api.libs.json._
+import play.api.libs.functional.syntax._
+
 import org.ergoplatform.appkit.{Address, BlockchainContext, ErgoClientException, InputBox, NetworkType, Parameters}
 import org.ergoplatform.appkit.config.ErgoToolConfig
 import utils.ErgoNamesUtils
 
 import scala.collection.JavaConverters._
 
-object ProcessMintingRequest {
+case class MintRequestSqsMessage(tokenDescription: String, mintRequestBoxId: String)
+
+
+trait Minter{
+  implicit val mintRequestSqsMessageReads = Json.reads[MintRequestSqsMessage]
+  implicit val mintRequestSqsMessageWrites = Json.writes[MintRequestSqsMessage]
 
   def createTx(ctx: BlockchainContext,
     inputs: java.util.List[InputBox],
@@ -14,7 +24,7 @@ object ProcessMintingRequest {
     mintRequestBox: InputBox,
     ergoNamesStandardTokenDescription: String,  networkType: NetworkType) = {
 
-         val issuanceBox = ErgoNamesUtils.buildBoxWithTokenToMint(
+        val issuanceBox = ErgoNamesUtils.buildBoxWithTokenToMint(
           ctx,
           networkType,
           value = Parameters.MinChangeValue,
@@ -38,14 +48,11 @@ object ProcessMintingRequest {
         tx
   }
 
-  def processMintingRequest(conf: ErgoToolConfig, networkType: NetworkType): String = {
-    val ergoClient = ErgoNamesUtils.buildErgoClient(conf.getNode, networkType)
-
-    val mintingContractAddress = Address.create(conf.getParameters.get("mintingContractAddress"))
-    val mintRequestBoxId = conf.getParameters.get("nftMintRequestBoxId")
-    val ergoNamesStandardTokenDescription = conf.getParameters.get("tokenDescription")
-
-    val txJson: String = ergoClient.execute((ctx: BlockchainContext) => {
+  def processMintingRequest(conf: ErgoToolConfig, networkType: NetworkType, mintContractAddress:String, mintRequestBoxId: String,  ergoNamesStandardTokenDescription: String): String = {
+    
+        val ergoClient = ErgoNamesUtils.buildErgoClient(conf.getNode, networkType)
+        val mintingContractAddress = Address.create(mintContractAddress)
+        val txJson: String = ergoClient.execute((ctx: BlockchainContext) => {
         val senderProver = ErgoNamesUtils.buildProver(ctx, conf.getNode)
 
         val mintRequestBox = ErgoNamesUtils.getMintRequestBox(ctx, mintingContractAddress, mintRequestBoxId)
@@ -56,8 +63,6 @@ object ProcessMintingRequest {
         val boxesToSpend = ErgoNamesUtils.getBoxesToSpendFromWallet(ctx, totalToSpend = Parameters.MinFee)
         if (!boxesToSpend.isPresent)
           throw new ErgoClientException(s"Not enough coins in the wallet to pay ${Parameters.MinFee}", null)
-
-
 
         // TODO: Move to ErgoNamesUtils
       val inputs = List.concat(List(mintRequestBox), boxesToSpend.get.asScala)
@@ -70,10 +75,40 @@ object ProcessMintingRequest {
     txJson
   }
 
+ /*
+  This function is an entrypoint for an AWS lambda consuming events form sqs.
+   - aws feeds a SQSEvent to this function.
+   - a SQSEvent contains multiple SQS messages
+   - this function expects each sqs message to be a json string describe by MintRequestSqsMessage
+   - this function parses each json sqs message into a MintRequestSqsMessage object
+   - then it feeds each MintRequestSqsMessage to the function which mints a domain
+ */
+  def lambdaEventHandler(sqsEvent: SQSEvent, context: Context) : Unit = {
+        val conf: ErgoToolConfig = ErgoToolConfig.load("config.json")
+        val networkType = if (conf.getNode.getNetworkType == "TESTNET") NetworkType.TESTNET else NetworkType.MAINNET
+       val mintingContractAddress = conf.getParameters.get("mintingContractAddress")
+       val sqsMessages = sqsEvent.getRecords().asScala
+       val mintRequests = sqsMessages.map(_.getBody())
+                                     .map(Json.parse(_).as[MintRequestSqsMessage])
+       mintRequests.map{
+              mintRequest =>
+             // ToDo handle failures here so that a single request failure does not taint the entire batch of sqs messages
+             processMintingRequest(conf, networkType, mintingContractAddress,
+             mintRequest.mintRequestBoxId, mintRequest.tokenDescription)
+             // ToDo delete sqs message if successful
+      }
+
+  }
+}
+
+object ProcessMintingRequest extends Minter {
   def main(args: Array[String]) : Unit = {
     val conf: ErgoToolConfig = ErgoToolConfig.load("config.json")
     val networkType = if (conf.getNode.getNetworkType == "TESTNET") NetworkType.TESTNET else NetworkType.MAINNET
-    val txJson = processMintingRequest(conf, networkType)
+    val tokenDesc = conf.getParameters.get("tokenDescription")
+    val mintRequestBoxId = conf.getParameters.get("nftMintRequestBoxId")
+    val mintingContractAddress = conf.getParameters.get("mintingContractAddress")
+    val txJson = processMintingRequest(conf, networkType, mintingContractAddress, mintRequestBoxId, tokenDesc)
     print(txJson)
   }
 }
