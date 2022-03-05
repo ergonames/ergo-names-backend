56411bcf86a26e28f3b283fbcea4c4ef43e455db
David Przybilla <dav.alejandro@gmail.com>
2022-03-05 22:25:55 +0900

    send sqs script

---
+++ scripts/send_sqs.sh
@@ -0,0 +1,1 @@
+aws sqs send-message --queue-url https://sqs.us-west-2.amazonaws.com/ACCOUNTNUMBER/mint-requests-queue-dev --message-body '{ "tokenDescription": "dummy_dec", "mintRequestBoxId": "1223"}'
