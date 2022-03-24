39a9f55b8004a3640961e1da5f8542aceb343663
JC Gonzalez <programmer1718@gmail.com>
2022-03-24 11:49:42 -0400

    refactor lambdaEventHandler
    - update the way it pulls config values
    - have it pull node config from aws secrets manager
    - enhance logging

--- src/main/scala/scenarios/ProcessMintingRequest.scala
+++ src/main/scala/scenarios/ProcessMintingRequest.scala
@@ -92,32 +92,71 @@ trait Minter {
    - then it feeds each MintRequestSqsMessage to the function which mints an ergoname NFT
  */
   def lambdaEventHandler(sqsEvent: SQSEvent, context: Context) : Unit = {
-        val config = Config.load("config.json")
-        val sqsClient = getSqsClient("us-west-2")
-        val conf: ErgoToolConfig = ErgoToolConfig.load("ergo_node_config.json")
-        val networkType = if (conf.getNode.getNetworkType == "TESTNET") NetworkType.TESTNET else NetworkType.MAINNET
-       val mintingContractAddress = conf.getParameters.get("mintingContractAddress")
-       val sqsMessages = sqsEvent.getRecords().asScala
-    val mintRequests = sqsMessages.map{ m =>
-      val parsed =  Json.parse(m.getBody()).as[MintRequestSqsMessage]
-      (m, parsed)
-    }
-                                     
-       mintRequests.map{
+        println("Getting AWS region")
+        val awsRegion = AwsHelper.getRegion
+        if (awsRegion == null)
+          throw new Exception("Could not find aws region")
+
+        println("Getting AWS Secrets Manager, secret name for Ergo node config")
+        val secretName = ConfigManager.get("secretName")
+        if (secretName == null)
+          throw new Exception("Could not find config value 'secretName'")
+        println(s"Getting secret $secretName from AWS Secrets Manager")
+        val secretString = AwsHelper.getSecretFromSecretsManager(secretName, awsRegion)
+
+        println("Parsing secret string into ErgoToolConfig type")
+        val ergoNodeConfig = ErgoToolConfig.load(new StringReader(secretString))
+
+        val networkType = ergoNodeConfig.getNode.getNetworkType
+        println(s"Targeting $networkType")
+
+        // TODO: Update minting method to take an ergoClient rather than a config to instantiate one
+        println(s"Building Ergo client for interacting with node at ${ergoNodeConfig.getNode.getNodeApi.getApiUrl}")
+        val ergoClient = ErgoNamesUtils.buildErgoClient(ergoNodeConfig.getNode, ergoNodeConfig.getNode.getNetworkType)
+
+        val mintingContractAddress = ergoNodeConfig.getParameters.get("mintingContractAddress")
+        println(s"Minting Contract Address: $mintingContractAddress")
+
+        val queueUrl = ConfigManager.get("mintRequestsQueueUrl")
+        if (queueUrl == null)
+          throw new Exception("Could not find config value 'mintRequestsQueue'")
+
+        println(s"Building AWS SQS Client for queue $queueUrl")
+        val sqsClient = getSqsClient(awsRegion)
+
+        val sqsMessages = sqsEvent.getRecords.asScala
+        println(s"Pulled ${sqsMessages.length} message(s) from queue")
+        println("Extracting and parsing mint request(s) from SQS message(s)")
+        val mintRequests = sqsMessages.map{ m =>
+          val parsed = Json.parse(m.getBody).as[MintRequestSqsMessage]
+          (m, parsed)
+        }
+
+        println("Checking if Lambda is running in dry mode")
+        val dry = ConfigManager.get("dry")
+        if (dry == null) throw new Exception("Could not find config flag 'dry'")
+        val dryMsg = if (dry.toBoolean)  "Running in dry mode - will not process mint request or attempt to query node"
+                     else "NOT Running in dry mode - will attempt to process minting request and query node"
+        println(dryMsg)
+
+        mintRequests.foreach{
          case (message, mintRequest) =>
            // ToDo handle failures here so that a single request failure does not taint the entire batch of sqs messages
-             println("mintRequest")
-             println(mintRequest)
-             if (!config.dry){
-               // ToDo avoid creating an ergoclinet per message
-               processMintingRequest(conf, networkType, mintingContractAddress,
+           println(s"mint request: $mintRequest")
+           if (!dry.toBoolean){
+            println(s"Attempting to process mint request box ${mintRequest.mintRequestBoxId} issued by mint tx ${mintRequest.mintTxId}")
+             // ToDo avoid creating an ergo client per message
+             // TODO: Update processMintingRequest to take Address instead of String
+             processMintingRequest(
+               ergoNodeConfig,
+               networkType,
+               mintingContractAddress,
                mintRequest.mintRequestBoxId,
-               mintRequest.tokenDescription)
-             }
-             sqsClient.deleteMessage(config.queueUrl, message.getReceiptHandle())
-             print("deleted message")
-      }
-
+               ergoNodeConfig.getParameters.get("ergoNamesTokenDescription"))
+           }
+           sqsClient.deleteMessage(queueUrl, message.getReceiptHandle)
+           print("Deleted message")
+       }
   }
 }
 
