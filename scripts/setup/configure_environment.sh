675b3a400540a04ddc52ddee7b0dd11bb164e5e0
JC Gonzalez <33580723+jaysee260@users.noreply.github.com>
2022-10-28 02:31:42 +0000

    move some files around

---
+++ scripts/setup/configure_environment.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+/bin/bash ./scripts/setup/install_aws_cli.sh
+/bin/bash ./scripts/setup/install_scala.sh
---
+++ scripts/setup/install_aws_cli.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Install aws cli
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+
+# Clean up
+rm awscliv2.zip
+rm -rf aws
